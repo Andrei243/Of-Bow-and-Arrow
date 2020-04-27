343b60221b7e4e991e2c3d23221107e82f152887
Calin <costea.calin@gmail.com>
2020-04-27 20:20:32 +0300

    Added save game

---
+++ Assets/Caln/Scripts/SaveLoad.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Calin.Scripts
+{
+    class SaveLoad : MonoBehaviour
+    {
+        public struct SaveObject
+        {
+            public SaveObject(int checkpoint, List<int> deadEnemies)
+            {
+                playerCheckpoint = checkpoint;
+                deadEnemiesIds = deadEnemies;
+            }
+            public int playerCheckpoint { get; set; }
+            public List<int> deadEnemiesIds { get; set; }
+        }
+        private SaveObject saveFile;
+
+        public SaveLoad()
+        {
+            saveFile = new SaveObject(0, new List<int>());
+        }
+
+        public void NewCheckpoint(int checkpointId)
+        {
+            if (checkpointId > saveFile.playerCheckpoint)
+            {
+                saveFile.playerCheckpoint = checkpointId;
+                saveFile.deadEnemiesIds.Clear();
+            }
+        }
+
+        public void enemyDied(int enemyId)
+        {
+            saveFile.deadEnemiesIds.Add(enemyId);
+        }
+
+        public void playerDied()
+        {
+            //return player to checkpoint
+            //reset enemies (saveFile.deadEmeniesIds)
+            saveFile.deadEnemiesIds.Clear();
+        }
+
+        public void saveJson()
+        {
+            string json = JsonUtility.ToJson(saveFile);
+            using (StreamWriter file = File.CreateText(@"./saveFile.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, json);
+            }
+        }
+
+        public void loadJson()
+        {
+            using (StreamReader r = new StreamReader("./saveFile.json"))
+            {
+                string json = r.ReadToEnd();
+                SaveObject save = JsonUtility.FromJson<SaveObject>(json);
+                saveFile.playerCheckpoint = save.playerCheckpoint;
+                saveFile.deadEnemiesIds.Clear();
+            }
+        }
+    }
+}
