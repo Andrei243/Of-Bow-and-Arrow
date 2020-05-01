935b36da270c697c07c0fc2a45003fec92466c92
Calin <costea.calin@gmail.com>
2020-05-01 14:58:14 +0300

    Main menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        File.Delete(@".\saveFile.json");
+        SceneManager.LoadScene(1);
+    }
+
+    public void ResumeGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
